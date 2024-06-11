22b600a9bb949ddb11b39e174200f80b86cc05a4
Cody <6558800+Bl3nd@users.noreply.github.com>
2024-06-11 12:16:45 -0600

    Add license for RSyntaxTextAreaHighlighterEx from RSyntaxTextArea

--- src/main/java/com/github/bl3nd/byteview/gui/resourceviewer/component/RSyntaxTextAreaHighlighterEx.java
+++ src/main/java/com/github/bl3nd/byteview/gui/resourceviewer/component/RSyntaxTextAreaHighlighterEx.java
@@ -1,25 +1,24 @@
 /*
- * MIT License
+ * BSD 3-Clause "New" or "Revised" License
  *
- * Copyright (c) 2024 Cody March
+ * Copyright (c) 2021, Robert Futrell All rights reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ * 		Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *		Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *		Neither the name of the author nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.github.bl3nd.byteview.gui.resourceviewer.component;
@@ -38,15 +37,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Bl3nd.
- * Date: 6/10/2024
+ * Extension from RSyntaxTextArea
  */
 public class RSyntaxTextAreaHighlighterEx extends RSyntaxTextAreaHighlighter {
 	private List<SyntaxLayeredHighlightInfoImpl> markedOccurrences = new ArrayList<>();
-	private static final Color DEFAULT_PARSER_NOTICE_COLOR	= Color.RED;
+	private static final Color DEFAULT_PARSER_NOTICE_COLOR = Color.RED;
 
 	public Object addMarkedOccurrenceHighlight(int start, int end,
-										SmartHighlightPainter p) throws BadLocationException {
+											   SmartHighlightPainter p) throws BadLocationException {
 		Document doc = textArea.getDocument();
 		TextUI mapper = textArea.getUI();
 		// Always layered highlights for marked occurrences.
@@ -56,7 +54,7 @@ public class RSyntaxTextAreaHighlighterEx extends RSyntaxTextAreaHighlighter {
 		// HACK: Use "end-1" to prevent chars the user types at the "end" of
 		// the highlight to be absorbed into the highlight (default Highlight
 		// behavior).
-		i.setEndOffset(doc.createPosition(end-1));
+		i.setEndOffset(doc.createPosition(end - 1));
 		markedOccurrences.add(i);
 		mapper.damageRange(textArea, start, end);
 		return i;
@@ -74,7 +72,7 @@ public class RSyntaxTextAreaHighlighterEx extends RSyntaxTextAreaHighlighter {
 	@Override
 	public void paintLayeredHighlights(Graphics g, int lineStart, int lineEnd,
 									   Shape viewBounds, JTextComponent editor, View view) {
-		paintListLayered(g, lineStart,lineEnd, viewBounds, editor, view, markedOccurrences);
+		paintListLayered(g, lineStart, lineEnd, viewBounds, editor, view, markedOccurrences);
 		super.paintLayeredHighlights(g, lineStart, lineEnd, viewBounds, editor, view);
 	}
 
@@ -86,9 +84,9 @@ public class RSyntaxTextAreaHighlighterEx extends RSyntaxTextAreaHighlighter {
 		@Override
 		public Color getColor() {
 			Color color = null;
-			if (notice!=null) {
+			if (notice != null) {
 				color = notice.getColor();
-				if (color==null) {
+				if (color == null) {
 					color = DEFAULT_PARSER_NOTICE_COLOR;
 				}
 			}
